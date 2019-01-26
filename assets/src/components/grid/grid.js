5f4ce15daa694b17b6ada7c88894931b3faaf3d7
traf72 <traf72@gmail.com>
2019-01-27 00:09:34 +0300

    Remove obsolete code

--- assets/src/components/grid/grid.js
+++ assets/src/components/grid/grid.js
@@ -104,9 +104,7 @@ export default class Grid extends React.PureComponent {
     }
 
     componentWillReceiveProps(props) {
-        if (!utils.isObjectsEquals(this.props, props)) {
-            this.setState(this._getStateFromProps(props));
-        }
+        this.setState(this._getStateFromProps(props));
     }
 
     componentWillUpdate() {
